a3c67f3a2549423a7a8a27b4675666c390b94fda
syakir <syakir@tuliprsb.com>
2026-01-13 16:43:56 +0800

    Fix minor bugs

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -6,6 +6,8 @@ import { notesOutline } from "./outline";
 
 type SearchParams = Record<string, string | string[] | undefined>;
 
+export const runtime = "edge";
+
 export default async function NotesPage({
   searchParams,
 }: {
